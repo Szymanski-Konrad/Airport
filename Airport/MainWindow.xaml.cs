e682bbd5470922d9f5865e7eef5f52e06c2d60e8
Konrad Szymański <konrad.szymanski07@gmail.com>
2019-01-24 12:52:14 +0100

    add switcher

--- Airport/MainWindow.xaml.cs
+++ Airport/MainWindow.xaml.cs
@@ -29,5 +29,21 @@ namespace Airport
             //NHiberControl.SaveGame();
             NHiberControl.LoadGames();
         }
+
+        public void Navigate(UserControl nextPage)
+        {
+            this.Content = nextPage;
+        }
+
+        public void Navigate(UserControl nextPage, object state)
+        {
+            this.Content = nextPage;
+            ISwitchable s = nextPage as ISwitchable;
+
+            if (s != null)
+                s.UtilizeState(state);
+            else
+                throw new ArgumentException("NextPage is not ISwitchable! " + nextPage.Name.ToString());
+        }
     }
 }
---
+++ Airport/Switcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Airport
+{
+    public static class Switcher
+    {
+        public static MainWindow pageSwitcher;
+
+        public static void Switch(UserControl page)
+        {
+            pageSwitcher.Navigate(page);
+        }
+
+        public static void Switch(UserControl newPage, object state)
+        {
+            pageSwitcher.Navigate(newPage, state);
+        }
+    }
+}
